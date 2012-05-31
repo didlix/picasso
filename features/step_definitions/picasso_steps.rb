3bd66c2fd9fb6273e96cf7e65c6dd6446c43c84e
Rachel Graves <rachel@didlix.co.uk>
2012-05-31 16:04:19 +0100

    Check if a canvas is the correct dimensions

--- features/step_definitions/picasso_steps.rb
+++ features/step_definitions/picasso_steps.rb
@@ -31,17 +31,28 @@ Given /^Picasso is running$/ do
   @picasso.start
 end
 
+When /^I try to create a canvas with the command "([^"]*)"$/ do |command|
+  @picasso.command(command)
+end
+
 When /^I enter the command "([^"]*)"$/ do |command|
   @picasso.command(command)
 end
 
 Then /^I should see a canvas with (\d+) columns and (\d+) rows$/ do |cols, rows|
-  canvas = output.messages
-  canvas.each do |message|
-    puts message
+  canvas = output.messages.each.select {|x| x =~ /^[A-Z]+$/ }
+  
+  # check rows of canvas
+  canvas.count.should == rows.to_i
+
+  # check cols of canvas
+  canvas.each do |row|
+    row.length.should == cols.to_i
   end
-  # canvas = output.messages.select {|x| x =~ /[A-Z]*/ }
+  
+  # and check total number of pixels is expected
   number_of_expected_pixels = cols.to_i * rows.to_i
+  canvas.to_s.length.should == number_of_expected_pixels
 end
 
 Then /^all pixels on that canvas should be "([^"]*)"$/ do |arg1|
