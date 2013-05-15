9287d180a87915aab51e14f8340c11318e603025
Rachel Graves <rachel@didlix.net>
2013-05-15 17:54:10 +0100

    Makes Colour return a string when initalized

--- lib/colour.rb
+++ lib/colour.rb
@@ -1,11 +1,14 @@
 class Picasso::App::Colour
-
   def initialize(colour)
     if colour.length == 1 && colour.match(/[A-Z]+/)
       @colour = colour
     else
       raise Picasso::InvalidColourException, "Can't have #{colour} as a colour"
     end
+  end
+
+  def inspect
+    to_s
   end
 
   def to_s
