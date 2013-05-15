debbbf10c39c0ed9f8b43d3258cb9ef7c0bc2781
Rachel Graves <rachel@didlix.co.uk>
2013-05-15 13:07:33 +0100

    Moves canvas into Picasso::App namespace, fixes indentation

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -1,39 +1,39 @@
 # Based on a 2D Array
-  class Canvas
+class Picasso::App::Canvas
 
-    attr_reader :rows, :cols
+  attr_reader :rows, :cols
 
-    def initialize(rows, cols, output)
-      # create 2D array setting each element to 'O'
-      @data = Array.new(rows) { Array.new(cols) { 'O' } }
-      
-      # Set number of rows and cols (remembering array starts at 0)
-      @rows = rows-1
-      @cols = cols-1
+  def initialize(rows, cols, output)
+    # create 2D array setting each element to 'O'
+    @data = Array.new(rows) { Array.new(cols) { 'O' } }
 
-      @output = output
-    end
+    # Set number of rows and cols (remembering array starts at 0)
+    @rows = rows-1
+    @cols = cols-1
 
-    def [](y, x)
-      @data[y][x]
-    end
+    @output = output
+  end
 
-    def []=(y, x, value)
-      if in_bounds?(y, x)
-        @data[y][x] = value
-      end
-    end
-    
-    def get_canvas
-      @data
-    end
+  def [](y, x)
+    @data[y][x]
+  end
 
-    def in_bounds?(y, x)
-      if y >= 0 && y <= @rows && x >= 0 && x <= @cols
-        true
-      else
-        @output.puts "Error: The pixel you tried to colour is out of range of the canvas"
-        false
-      end
+  def []=(y, x, value)
+    if in_bounds?(y, x)
+      @data[y][x] = value
     end
   end
+
+  def get_canvas
+    @data
+  end
+
+  def in_bounds?(y, x)
+    if y >= 0 && y <= @rows && x >= 0 && x <= @cols
+      true
+    else
+      @output.puts "Error: The pixel you tried to colour is out of range of the canvas"
+      false
+    end
+  end
+end
