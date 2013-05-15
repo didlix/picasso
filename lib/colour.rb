9f8334a042fef80c6a19a0e26126988eb3fb181e
Rachel Graves <rachel@didlix.co.uk>
2013-05-15 12:19:03 +0100

    Adds a colour object

---
+++ lib/colour.rb
@@ -0,0 +1,15 @@
+class colour
+
+  def initalize(colour)
+    if colour.length == 1 && colour.match(/[A-Z]+/)
+      @colour = colour
+    else
+      @output.puts "Error: That is not a valid colour, valid colours are A-Z"
+      false
+    end
+  end
+
+  def to_s
+    @colour
+  end
+end
