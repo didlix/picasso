52305e2c365f3d06a65406546f36326e4e8af7b6
Rachel Graves <rachel@didlix.co.uk>
2013-05-15 13:03:58 +0100

    Colour raises an exception if an invalid colour is entered

--- lib/colour.rb
+++ lib/colour.rb
@@ -1,11 +1,10 @@
-class Colour
+class Picasso::App::Colour
 
   def initialize(colour)
     if colour.length == 1 && colour.match(/[A-Z]+/)
       @colour = colour
     else
-      @output.puts "Error: That is not a valid colour, valid colours are A-Z"
-      false
+      raise Picasso::InvalidColourException, "Can't have #{colour} as a colour"
     end
   end
 
